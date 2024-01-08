68421ff18ad5516796d6beb20ed6a038a53cb48f
1zumisawashun <shun.1zumisawa@gmail.com>
2024-01-08 21:22:19 +0900

    fix css

--- app/functions/types/Common.ts
+++ app/functions/types/Common.ts
@@ -45,3 +45,51 @@ export type StackPosition = `${"top" | "bottom" | "right" | "left"}${
   | "Left"
   | "Center"
   | "Right"}`;
+
+/*
+ * @see https://twitter.com/mattpocockuk/status/1740418559623508269?s=12&t=0Bs_ltBYiO3nhiiL9YZAEw
+ */
+type InferValueFromColor<Color extends string> =
+  Color extends `${infer N}-${infer C}-${infer T}`
+    ? {
+        namespace: N;
+        color: C;
+        tone: T;
+      }
+    : never;
+
+type Example = InferValueFromColor<"text-red-500">;
+
+/*
+ * @see https://twitter.com/mattpocockuk/status/1740702711652225157?s=12&t=0Bs_ltBYiO3nhiiL9YZAEw
+ */
+const inferValueFromColor = <
+  N extends string,
+  C extends string,
+  T extends number
+>(
+  colorTag: `${N}-${C}-${T}`
+) => {
+  const [namespace, color, tone] = colorTag.split("-");
+  return {
+    namespace: namespace as N,
+    color: color as C,
+    tone: Number(tone) as T,
+  };
+};
+
+const example = inferValueFromColor("text-red-500");
+
+type A = "foo"; // global scope
+type B = A extends infer C
+  ? C extends "foo"
+    ? true
+    : false // *only* inside this expression, C represents A
+  : never;
+
+type Fruit = "apple" | "banana" | "orange";
+
+// I really wish that Omit and Pick were called OmitKeys and PickKeys instead.
+// @see https://twitter.com/mattpocockuk/status/1691402204798746624?s=12&t=0Bs_ltBYiO3nhiiL9YZAEw
+// type CitrusFruit = Omit<Fruit, "apple">;
+type CitrusFruit = Exclude<Fruit, "apple">;
